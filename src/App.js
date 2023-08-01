3ef3382385407214beee483599834ef89fb8f882
Egon Dohr <egondohr03@gmail.com>
2023-08-01 15:52:15 +0300

    Sort moves button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [indexHistory, setIndexHistory] = useState([]);
+  const [sortAscending, setSortAscending] = useState(true)
   const currentSquares = history[currentMove];
   const xIsNext = currentMove % 2 === 0;
 
@@ -21,19 +22,21 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    let index = indexHistory[move]
-    description = "Go to move #" + move + ` X: ${Math.floor(index / 3) + 1} Y: ${index % 3 + 1}`;
-
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
     return (
       <li key={move}>
-        {move === currentMove ? (
-          <span>You are at move #{move}</span>
-        ) : (
-          <button onClick={() => jumpTo(move)}>{description}</button>
-        )}
+        <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
+
+  if (!sortAscending) {
+    moves.reverse()
+  }
 
   return (
     <div className="game">
@@ -42,6 +45,11 @@ export default function Game() {
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
+        <ol>
+          <button onClick={() => setSortAscending(!sortAscending)}>
+            Toggle Sort ({sortAscending ? 'Descending' : 'Ascending'})
+          </button>
+        </ol>
       </div>
     </div>
   );
